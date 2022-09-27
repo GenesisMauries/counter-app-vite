6e8ba649579753b5612bdd242725cbf26ffb1a90
GenesisMauries <exodo_54@hotmail.com>
2022-09-27 17:40:08 -0500

    Usando useState en CounterApp.jsx

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
-// const handleAdd = (event) => console.log("Mas uno", event); // Fuera porque no tiene nada que ver con el componente y para que no se le llame cada que renderiza el componente
+
 export const CounterApp = ({ value }) => {
-  const handleAdd = (event, value) => console.log("Mas uno", event, value);
+  // useState necesita el valor y la funcion que modificara el estado
+  const [counter, setCounter] = useState(value);
+  const handleAdd = () => {
+    setCounter(counter + 2);
+    // setCounter((c) => c + 10); // Cambiar el estado con callback
+  };
   return (
     <>
       <h1>Counter App</h1>
-      <h2> {value} </h2>
-      {/* Evento */}
-      {/* <button onClick={handleAdd}>+1</button> */}
-      {/* Evento + de un argumento */}
-      <button onClick={(event) => handleAdd(event, value)}>+1</button>
+      <h2> {counter} </h2>
+
+      <button onClick={handleAdd}>+1</button>
     </>
   );
 };
